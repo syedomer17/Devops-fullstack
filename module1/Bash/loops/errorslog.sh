e5ac20718911ed2ec8719447642841d13e4774f1
syedomer17 <syedomerali2006@gmail.com>
2025-09-08 20:20:55 +0530

    Updated by Omer on 2025-09-08 at 08:20PM

--- module1/Bash/loops/errorslog.sh
+++ module1/Bash/loops/errorslog.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-input_file=/home/omar/syslog
-output_file=/home/omar/errror.log
+input_file=/var/log/syslog
+output_file=/home/omar/Devops-fullstack/module1/Bash/loops/error.log
 
 if [[ -f $input_file ]]
 then
-echo "file isavailable"
+echo "file is available and also available for reading the logs at $output_file"
 sudo egrep -i "(error|critical|emergency)" $input_file > $output_file
 else
 echo "file is not available"
